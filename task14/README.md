98da2b1336a03caf6df9e381ae0e90d6a9be83e7
antonkhmv <antonkhmv@gmail.com>
2021-03-26 09:07:48 +0300

    Task 14

---
+++ task14/README.md
@@ -0,0 +1,28 @@
+# task14
+### Код 
+
+* **Файл ресурсов**
+
+![str](https://github.com/antonkhmv/android_dz/blob/main/task14/img/str.png)
+
+* **Main Activity Code**
+
+![main](https://github.com/antonkhmv/android_dz/blob/main/task14/img/main_code.png)
+
+* **Main Activity Layout**
+
+![main_lay](https://github.com/antonkhmv/android_dz/blob/main/task14/img/lay.png)
+
+* **Manifest**
+
+![sec_lay](https://github.com/antonkhmv/android_dz/blob/main/task14/img/man.png)
+ 
+### Результат
+
+![res](https://github.com/antonkhmv/android_dz/blob/main/task14/img/res.gif)
+
+## [Следующее задание task15](../task15)
+
+## [На главную](/../../)
+
+
---
+++ task14/Task14/app/src/main/java/ru/hse/task14/MainActivity.java
@@ -0,0 +1,96 @@
+package ru.hse.task14;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    private boolean checkPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                200);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.button);
+
+        requestPermission();
+
+        if (checkPermission()) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 0);
+                }
+            });
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.about) {
+            AlertDialog.Builder dialog = new
+                    AlertDialog.Builder(MainActivity.this);
+            dialog.setMessage(getString(R.string.fio));
+            dialog.setTitle("О программе");
+            dialog.setNeutralButton("OK", new
+                    DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            dialog.setIcon(R.mipmap.ic_launcher_round);
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
